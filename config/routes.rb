7e680605cfe7c3f08ca18984529586d531ac31c3
Alice Clavel <alice.claouis@gmail.com>
2018-02-13 17:47:50 +0100

    Feat: Add routes for pets CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pets', to: 'pets#index'
+  get 'pets/:id', to: 'pets#show', as: :pet
+
+  get 'pets/new', to: 'pets#new', as: :new_pet 
+  post 'pets', to: 'pets#create'
+
+  
 end
